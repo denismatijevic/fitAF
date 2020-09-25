f1f3a7de0118446be0e48cf8f39dffdf14c0a675
denis matijevic <denisone90@gmail.com>
2020-09-24 22:39:26 -0600

    made my exercises into an array

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,30 +1,39 @@
-const {model, Schema} = require("mongoose");
+const { model, Schema } = require("mongoose");
 
-const workoutSchema = new Schema ({
-  name: {
-    type: String,
-    require: true
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date()
   },
-  type: {
-    type: String,
-    require: true
-  },
-  weight: {
-    type: String,
-    require: true
-  },
-  set: {
-    type: String,
-    require: true
-  },
-  reps: {
-    type: String,
-    require: true
-  },
-  duration: {
-    type: Number
-  }
+  //  make an exercises array [{add exercises here}]
+  exercises: [
+    {
+      name: {
+        type: String,
+        require: true
+      },
+      type: {
+        type: String,
+        require: true
+      },
+      weight: {
+        type: String,
+        require: true
+      },
+      set: {
+        type: String,
+        require: true
+      },
+      reps: {
+        type: String,
+        require: true
+      },
+      duration: {
+        type: Number
+      }
+    }
+  ]
 })
-const Workout = model("Workout", workoutSchema) 
+const Workout = model("Workout", workoutSchema)
 
 module.exports = Workout;
