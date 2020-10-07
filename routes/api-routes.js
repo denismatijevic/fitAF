ba31f2d1f83bc8e5f8a0d697468c5f555848ffee
denis matijevic <denisone90@gmail.com>
2020-10-06 21:46:30 -0600

    added comment

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const workoutController = require("../controller/workoutController");
-
+// database 
 router.get("/workouts", workoutController.getWorkout);
 
 router.post ("/workouts", workoutController.addWorkout);
